fa96c09af76bbffb5eff7f79c253607aaf7a27d8
Mia Morticia <miamorticia@Mias-MacBook-Pro.local>
2021-02-23 13:57:14 -0700

    building out the response

--- src/components/NasaPhoto.js
+++ src/components/NasaPhoto.js
@@ -1,7 +1,58 @@
 import React, { useState, useEffect } from "react";
-import NavBar from "./NavBar";
+//import NavBar from "./NavBar";
 
+// const apiKey = process.env.REACT_APP_NASA_KEY;
 
 export default function NasaPhoto() {
+    const [photoData, setPhotoData] = useState(null);
+
+    // Grab the pic from the API
+     useEffect(() => {
+         fetchPhoto();
     
-}
+        async function fetchPhoto() {
+          const res = await fetch(
+            `https://api.nasa.gov/planetary/apod?api_key=`
+          );
+           const data = await res.json();
+           //setting state 
+           setPhotoData(data);
+           console.log(data);
+         }
+      }, []);
+
+      if (!photoData) return <div />;
+
+
+ return (
+     <>
+     {/* <NavBar /> */}
+    {/* <div className="nasa-photo"> */}
+    <div>
+       {photoData.media_type === "image" ? (
+        <img
+          src={photoData.url}
+          alt={photoData.title}
+          //className="photo"
+        />
+       ) : (
+         // if API returns a video
+        <iframe
+          title="space-video"
+          src={photoData.url}
+          frameBorder="0"
+          gesture="media"
+          allow="encrypted-media"
+          allowFullScreen
+          className="photo"
+        />
+      )}
+      <div>
+        <h1>{photoData.title}</h1>
+        <p className="date">{photoData.date}</p>
+        <p className="explanation">{photoData.explanation}</p>
+      </div>
+     </div>
+     </>
+   );
+       }
