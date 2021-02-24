3bb86c80531de1a85458c5217c08256d84ecf35e
Mia Morticia <miamorticia@Mias-MacBook-Pro.local>
2021-02-23 18:22:49 -0700

    extra styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import ReactPlayer from 'react-player';
+
 
 
 export default function Home() {
   return (
       <div className="home">
           <Link className="home-link" to="/nasaphoto">Star Gaze!</Link>
-          <ReactPlayer 
-              className="video"
-              url="video/space.mp4"
-              loop="true"
-              playing="true"
-              muted="true"
-              width="720"
-              height="480"
-          />
+
       </div>
      
   )
